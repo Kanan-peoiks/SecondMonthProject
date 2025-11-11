dab43f9d3a7962d237d70e840a44bf78ac892390
Kanan-peoiks <kenanshukurzadee@gmail.com>
2025-11-12 01:18:24 +0400

    commit 5

--- src/service/InventoryService.java
+++ src/service/InventoryService.java
@@ -1,4 +1,8 @@
 package service;
 
 public class InventoryService {
+    public boolean isInStock(String productId, int quantity) {
+        // Hazırda sadəcə true qaytarır
+        return true;
+    }
 }
